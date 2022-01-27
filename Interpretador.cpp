8e0d6fe9cb233d0bda1e0fe6204ad1c6cf94e12a
Ítalo Vinícius de Freitas Dias <italo.dias.017@ufrn.edu.br>
2022-01-27 15:34:46 -0300

    Trocando ifs

--- Interpretador.cpp
+++ Interpretador.cpp
@@ -43,41 +43,39 @@ vector<FiguraGeometrica *> Interpretador::interpretarArquivos(std::string nomeAr
                 if(t.compare("dim")==0){
                     stringStream>>dimx>>dimy>>dimz;
                 }
-                
-            }
+                if (t.compare("putBox")==0 ) {
+                    int xi,xf, yi,yf, zi,zf;
+                    stringStream >> xi>>xf >> yi>>yf >> zi>>zf >> r >> g >> b >> a;
+                    figuras.push_back(new PutBox(xi,xf, yi,yf, zi,zf,r,g,b,a));
+                }
+                if (t.compare("cutBox")==0 ) {
+                    int xi,xf, yi,yf, zi,zf;
+                    stringStream >> xi>>xf >> yi>>yf >> zi>>zf;
+                    figuras.push_back(new CutBox(xi,xf, yi,yf, zi,zf));
+                }
             
-            if (t.compare("putBox")==0 ) {
-                int xi,xf, yi,yf, zi,zf;
-                stringStream >> xi>>xf >> yi>>yf >> zi>>zf >> r >> g >> b >> a;
-                figuras.push_back(new PutBox(xi,xf, yi,yf, zi,zf,r,g,b,a));
-            }
-            if (t.compare("cutBox")==0 ) {
-                int xi,xf, yi,yf, zi,zf;
-                stringStream >> xi>>xf >> yi>>yf >> zi>>zf;
-                figuras.push_back(new CutBox(xi,xf, yi,yf, zi,zf));
-            }
+                if (t.compare("putSphere")==0 ) {
+                    int xc,yc, zc,raio;
+                    stringStream >> xc>>yc >> zc>>raio>>r >> g>>b>>a;
+                    figuras.push_back(new PutSphere(xc,yc,zc,raio,r,g,b,a));
+                }
+                if (t.compare("cutSphere")==0 ) {
+                    int xc,yc, zc,raio;
+                    stringStream >> xc>>yc >> zc>>raio;
+                    figuras.push_back(new CutSphere(xc,yc,zc,raio));
+                }
             
-            if (t.compare("putSphere")==0 ) {
-                int xc,yc, zc,raio;
-                stringStream >> xc>>yc >> zc>>raio>>r >> g>>b>>a;
-                figuras.push_back(new PutSphere(xc,yc,zc,raio,r,g,b,a));
-            }
-            if (t.compare("cutSphere")==0 ) {
-                int xc,yc, zc,raio;
-                stringStream >> xc>>yc >> zc>>raio;
-                figuras.push_back(new CutSphere(xc,yc,zc,raio));
-            }
+                if (t.compare("putEllipsoid")==0 ) {
+                    int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
+                    stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz>>r >> g>>b>>a;
+                    figuras.push_back(new PutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz,r,g,b,a));
+                }
             
-            if (t.compare("putEllipsoid")==0 ) {
-                int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
-                stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz>>r >> g>>b>>a;
-                figuras.push_back(new PutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz,r,g,b,a));
-            }
-            
-            if (t.compare("cutEllipsoid")==0 ) {
-                int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
-                stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz;
-                figuras.push_back(new CutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz));
+                if (t.compare("cutEllipsoid")==0 ) {
+                    int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
+                    stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz;
+                    figuras.push_back(new CutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz));
+                }
             }
         }
     }
