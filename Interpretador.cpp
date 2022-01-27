73bd1258082dc42bb069e508ac9e941fe27c5d69
Ítalo Vinícius de Freitas Dias <italo.dias.017@ufrn.edu.br>
2022-01-27 17:11:30 -0300

    Atualizando a implementação

--- Interpretador.cpp
+++ Interpretador.cpp
@@ -33,48 +33,46 @@ vector<FiguraGeometrica *> Interpretador::interpretarArquivos(std::string nomeAr
         std::cout<<"Erro ao abrir o arquivo\n";
         exit(0);
     }
-    while(fileIn.good()){
-        if(std::getline(fileIn,lineString)){
-            stringStream.clear();
-            stringStream.str(lineString);
-            stringStream>>operacao;
-            if(stringStream.good()){
-                if(operacao.compare("dim")==0){
-                    stringStream>>dimx>>dimy>>dimz;
-                }
-                if (operacao.compare("putBox")==0 ) {
-                    int xi,xf, yi,yf, zi,zf;
-                    stringStream >> xi>>xf >> yi>>yf >> zi>>zf >> r >> g >> b >> a;
-                    figuras.push_back(new PutBox(xi,xf, yi,yf, zi,zf,r,g,b,a));
-                }
-                if (operacao.compare("cutBox")==0 ) {
-                    int xi,xf, yi,yf, zi,zf;
-                    stringStream >> xi>>xf >> yi>>yf >> zi>>zf;
-                    figuras.push_back(new CutBox(xi,xf, yi,yf, zi,zf));
-                }
-            
-                if (operacao.compare("putSphere")==0 ) {
-                    int xc,yc, zc,raio;
-                    stringStream >> xc>>yc >> zc>>raio>>r >> g>>b>>a;
-                    figuras.push_back(new PutSphere(xc,yc,zc,raio,r,g,b,a));
-                }
-                if (operacao.compare("cutSphere")==0 ) {
-                    int xc,yc, zc,raio;
-                    stringStream >> xc>>yc >> zc>>raio;
-                    figuras.push_back(new CutSphere(xc,yc,zc,raio));
-                }
-            
-                if (operacao.compare("putEllipsoid")==0 ) {
-                    int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
-                    stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz>>r >> g>>b>>a;
-                    figuras.push_back(new PutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz,r,g,b,a));
-                }
-            
-                if (operacao.compare("cutEllipsoid")==0 ) {
-                    int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
-                    stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz;
-                    figuras.push_back(new CutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz));
-                }
+    while(std::getline(fileIn,lineString)){
+        stringStream.clear();
+        stringStream.str(lineString);
+        stringStream>>operacao;
+        if(stringStream.good()){
+            if(operacao.compare("dim")==0){
+                stringStream>>dimx>>dimy>>dimz;
+            }
+            if (operacao.compare("putBox")==0 ) {
+                int xi,xf, yi,yf, zi,zf;
+                stringStream >> xi>>xf >> yi>>yf >> zi>>zf >> r >> g >> b >> a;
+                figuras.push_back(new PutBox(xi,xf, yi,yf, zi,zf,r,g,b,a));
+            }
+            if (operacao.compare("cutBox")==0 ) {
+                int xi,xf, yi,yf, zi,zf;
+                stringStream >> xi>>xf >> yi>>yf >> zi>>zf;
+                figuras.push_back(new CutBox(xi,xf, yi,yf, zi,zf));
+            }
+
+            if (operacao.compare("putSphere")==0 ) {
+                int xc,yc, zc,raio;
+                stringStream >> xc>>yc >> zc>>raio>>r >> g>>b>>a;
+                figuras.push_back(new PutSphere(xc,yc,zc,raio,r,g,b,a));
+            }
+            if (operacao.compare("cutSphere")==0 ) {
+                int xc,yc, zc,raio;
+                stringStream >> xc>>yc >> zc>>raio;
+                figuras.push_back(new CutSphere(xc,yc,zc,raio));
+            }
+
+            if (operacao.compare("putEllipsoid")==0 ) {
+                int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
+                stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz>>r >> g>>b>>a;
+                figuras.push_back(new PutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz,r,g,b,a));
+            }
+
+            if (operacao.compare("cutEllipsoid")==0 ) {
+                int xcenter,  ycenter,  zcenter,  rx,  ry,  rz;
+                stringStream >> xcenter>>ycenter >> zcenter>> rx >> ry >> rz;
+                figuras.push_back(new CutEllipsoid(xcenter,  ycenter,  zcenter,  rx,  ry,  rz));
             }
         }
     }
